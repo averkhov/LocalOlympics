8faa3fe1ba053ea8be9a63203a5552272a4b12a2
kb <ait390s1401karen@gmail.com>
2014-02-20 22:19:26 -0500

    added deleteactivity servlet

---
+++ src/localolympics/servlet/DeleteActivityServlet.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2014 -
+ * Licensed under the Academic Free License version 3.0
+ * http://opensource.org/licenses/AFL-3.0
+ * 
+ * Authors: Karen Bacon
+ */
+
+package localolympics.servlet;
+
+import javax.servlet.http.*;
+import java.io.IOException;
+
+import localolympics.db.Activity;
+
+
+
+public class DeleteActivityServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String id = req.getParameter("id");
+        Activity.deleteActivity(id);
+        resp.sendRedirect("/admin/allActivity.jsp");
+		
+    }
+	
+	@Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String id = req.getParameter("id");
+        Activity.deleteActivity(id);
+        resp.sendRedirect("/admin/allActivity.jsp");
+		
+    }
+	
+
+}
+
+	
