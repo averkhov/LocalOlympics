983e889d971b7b730ca8464bc53d37333f0bc9ed
kb <ait390s1401karen@gmail.com>
2014-02-25 13:53:57 -0500

    added updateservlet for activity

---
+++ src/localolympics/servlet/UpdateActivityServlet.java
@@ -0,0 +1,31 @@
+/**
+* Copyright 2014 -
+* Licensed under the Academic Free License version 3.0
+* http://opensource.org/licenses/AFL-3.0
+*
+* Authors: Karen Bacon
+*/
+
+package localolympics.servlet;
+
+import javax.servlet.http.*;
+import java.io.IOException;
+
+import localolympics.db.Activity;
+
+
+
+public class UpdateActivityServlet extends HttpServlet {
+private static final long serialVersionUID = 1L;
+
+
+@Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+String id = req.getParameter("activityID");
+String activityName = req.getParameter("activityName");
+        Activity.UpdateActivity(id, activityName);
+        resp.sendRedirect("/admin/allActivity.jsp");
+    }
+
+
+});
